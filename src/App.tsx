013f3ca0f982c9ef95c271e8cd8517a68c1efdb5
NarenderSingh <naren.delhi13@gmail.com>
2020-04-18 23:19:38 +0530

    Initial Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClassCounter from "./components/ClassCounter";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ClassCounter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ClassCounter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ClassCounter = () => (
+  <div>
+    <h2>Hello World</h2>
+  </div>
+);
+
+export default ClassCounter;
